dc77e7facdd6990bb9c9ddfb77371693e40ad6cf
Yihang Li <asjjforever@gmail.com>
2025-01-02 11:28:46 +0800

    fix: Remove duplicate task form and ensure single task input form - Removed the extra `addTask` form that was unintentionally duplicated

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
-    <task-form />
     <task-list />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import TaskForm from '../components/TaskForm.vue';
 import TaskList from '../components/TaskList.vue';
 
 export default defineComponent({
-  components: { TaskForm, TaskList },
+  components: { TaskList },
 });
 </script>
 
