c5c14971dad66826e8591d7c1d4b178926fa79ec
Yihang Li <asjjforever@gmail.com>
2025-01-06 11:09:39 +0800

    bugfix: duplicate uuid -Replace uuid by mongodb default _id

--- backend/models/task.model.js
+++ backend/models/task.model.js
@@ -1,13 +1,11 @@
-const { v4: uuidv4 } = require('uuid');
 const mongoose = require('mongoose');
 
 const taskSchema = mongoose.Schema({
-    id:{type: String, required: true,default: uuidv4()},
     name: { type: String, required: true },
     description: { type: String },
-    catFact:{ type: String },
-    status:{ type: String, default: 'pending'},
-    createAt:{type: Date, default: Date.now},
+    catFact: { type: String },
+    status: { type: String, default: 'pending' },
+    createAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('tasks', taskSchema);
